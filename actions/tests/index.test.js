b8be157ea0efd8f4bf74747a590240e5698e7885
HaYa Abu Al Halawa <44801508+hayaah@users.noreply.github.com>
2022-02-10 11:05:58 +0200

    Update index.test.js

--- actions/tests/index.test.js
+++ actions/tests/index.test.js
@@ -84,7 +84,7 @@ describe("test calling update module version api", () => {
     done()
   })
 
-  test('check if the url is set correctly!!!!', async (done) => {
+  test('check if the url is set correctly 99', async (done) => {
     setGithubInput("organization", "infragod-test")
     setGithubInput("registry_name", "private")
     setGithubInput("provider", "gcp")
